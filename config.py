cd3d78d59a289fcda5d6ed36778e9b2836f70184
HectraCore <hectracore@gmail.com>
2026-04-09 03:07:03 +0400

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config:
     API_ID = int(os.getenv("API_ID", 0))
     API_HASH = os.getenv("API_HASH")
 
-    MAIN_URI = os.getenv("")
+    MAIN_URI = os.getenv("MAIN_URI")
     DB_NAME = "MainDB"
     SETTINGS_COLLECTION = "MediaStudio-Settings"
 
